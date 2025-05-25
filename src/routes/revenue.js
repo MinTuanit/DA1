56549c95e1f1150a3c9caac5ff083c2817fd73ff
nukota <22521351@gm.uit.edu.vn>
2025-05-25 22:01:18 +0700

    change revenue api endpoints get to post

--- src/routes/revenue.js
+++ src/routes/revenue.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const revenuecontroller = require("../controllers/revenuecontroller");
 
 router.get("/", revenuecontroller.getAll);
-router.get("/all", revenuecontroller.getAllRevenueReport);
-router.get("/date/product", revenuecontroller.getDailyProductSalesByProduct);
-router.get("/date/movie", revenuecontroller.getDailyTicketRevenueByMovie);
+router.post("/all", revenuecontroller.getAllRevenueReport);
+router.post("/date/product", revenuecontroller.getDailyProductSalesByProduct);
+router.post("/date/movie", revenuecontroller.getDailyTicketRevenueByMovie);
 
 module.exports = router;
