051232e62f77508379a337884cd41fe5eccb077d
MinTuan2124 <166462832+MinTuanit@users.noreply.github.com>
2025-05-25 14:49:35 +0700

    Update revenue.js

--- src/routes/revenue.js
+++ src/routes/revenue.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const revenuecontroller = require("../controllers/revenuecontroller");
 
 router.get("/", revenuecontroller.getAll);
-router.get("/all", revenuecontroller.getRevenueReport);
+router.get("/all", revenuecontroller.getAllRevenueReport);
 router.get("/date/product", revenuecontroller.getDailyProductSalesByProduct);
 router.get("/date/movie", revenuecontroller.getDailyTicketRevenueByMovie);
 
